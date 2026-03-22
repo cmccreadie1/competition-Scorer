efdac57dcd43748625bb3cd3e76922273846a0ab
Sea Catch Scorer <50778220+cmccreadie1@users.noreply.github.com>
2026-03-22 19:10:41 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 /* SEA DIARY: MATCH EDITION 
-   VERSION 4.4.4 - THE ADMIN SEPARATION
+   VERSION 4.5.0 - SLEEK UI & PRECISION SCORING
    FULL VOLUME SERVICE WORKER
 */
 
-const CACHE_NAME = 'match-edition-v4.4.4-gold';
+const CACHE_NAME = 'match-edition-v4.5.0-gold';
 
 const ASSETS = [
   './',
@@ -19,7 +19,7 @@ self.addEventListener('install', (event) => {
   
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('SW: Caching Gold Master 4.4.4 Assets');
+      console.log('SW: Caching Gold Master 4.5.0 Assets');
       return cache.addAll(ASSETS);
     })
   );
