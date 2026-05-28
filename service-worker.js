fe10352cfcce23d9b7664fa401b33d19b701d0ec
Sea Catch Scorer <50778220+cmccreadie1@users.noreply.github.com>
2026-05-28 20:08:04 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-/* ShoreScore V10.6.1 Service Worker */
-const CACHE_NAME = 'shorescore-v10.6.1';
+/* ShoreScore V10.7.0 Service Worker */
+const CACHE_NAME = 'shorescore-v10.7.0';
 
 self.addEventListener('install', event => {
     self.skipWaiting(); 
