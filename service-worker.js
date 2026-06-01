9d718f0b06dee9205020c95fc0413bd97184c8d9
Sea Catch Scorer <50778220+cmccreadie1@users.noreply.github.com>
2026-06-01 11:35:49 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // ================================================================================
-// SHORESCORE v15.1.0 CORE OFFLINE SEAMLESS SERVICE WORKER INFRASTRUCTURE
+// SHORESCORE v15.2.0 CORE OFFLINE SEAMLESS SERVICE WORKER INFRASTRUCTURE
 // ================================================================================
-const CACHE_IDENTIFIER = 'shorescore-v15.1.0-matrix-cache';
+const CACHE_IDENTIFIER = 'shorescore-v15.2.0-matrix-cache';
 const STATIC_ASSET_MANIFEST = [
   './',
   './index.html',
